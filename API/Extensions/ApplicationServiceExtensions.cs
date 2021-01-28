5c37e14950c7164a33ec93969279662d0401dc2e
Oleg <kerki2000@gmail.com>
2021-01-28 00:00:00 -0500

    End of section 4

---
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -0,0 +1,23 @@
+using API.Data;
+using API.Interfaces;
+using API.Services;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace API.Extensions
+{
+    public static class ApplicationServiceExtensions
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration config)
+        {
+            services.AddScoped<ITokenService, TokenService>(); // lifetime of the http request ,this service injected into controller,
+            //then new instance of this service is created and when request is finished, the service is disposed
+            services.AddDbContext<DataContext>(options =>
+            {
+                options.UseSqlite(config.GetConnectionString("DefaultConnection"));
+            });
+            return services;
+        }
+    }
+}
