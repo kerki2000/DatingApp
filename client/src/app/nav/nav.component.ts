20ece24406477a6b4fe0ac959df8b4bad8410759
Oleg <kerki2000@gmail.com>
2021-01-31 11:43:58 -0500

    End of section 6

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { Observable } from 'rxjs';
+import { User } from '../_models/user';
 import { AccountService } from '../_services/account.service';
 
 @Component({
@@ -8,35 +12,32 @@ import { AccountService } from '../_services/account.service';
 })
 export class NavComponent implements OnInit {
   model: any = {}
-  loggedIn: boolean;
+ 
 
-  constructor(private accountService: AccountService) { }
+  constructor(public accountService: AccountService, private router: Router, private toastr: ToastrService) { }
 
   ngOnInit(): void {
-    this.getCurrentUser();
+    
 
   }
   login(){
     //console.log(this.model);
     this.accountService.login(this.model).subscribe(response => {
-      console.log(response);
-      this.loggedIn = true;
+      //console.log(response);
+      this.router.navigateByUrl('/members');
+    
     }, error => {
       console.log(error);
+      this.toastr.error(error.error);// to see the reason of the error
     })
   }
-    getCurrentUser() {
-      this.accountService.currentUser$.subscribe(user => {
-        this.loggedIn = !!user;
-      }, error => {
-        console.log(error);
-      })
-    }
+   
 
 
   logout() {
     this.accountService.logout();
-    this.loggedIn = false;
+    this.router.navigateByUrl('/');
+   // this.loggedIn = false;
   }
 
 }
